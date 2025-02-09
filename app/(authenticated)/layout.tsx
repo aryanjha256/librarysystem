d34ff59a9d17eb1aeaa40aa2772240ff2b3145f1
Aryan Kumar <aryanajha256@gmail.com>
2025-02-09 15:34:59 +0530

    redirects

--- app/(authenticated)/layout.tsx
+++ app/(authenticated)/layout.tsx
@@ -3,14 +3,18 @@ import { AppSidebar } from "@/components/app-sidebar";
 import Header from "@/components/header";
 import { SidebarInset, SidebarProvider } from "@/components/ui/sidebar";
 import { useUserStore } from "@/hooks/store/use-store";
+import { useRouter } from "next/navigation";
 import React from "react";
 
 const MainLayout = ({ children }: { children: React.ReactNode }) => {
   const setUser = useUserStore((state) => state.setUser);
   const userData = sessionStorage.getItem("userData");
+  const router = useRouter();
 
   if (userData) {
     setUser(JSON.parse(userData));
+  } else {
+    router.push("/auth");
   }
 
   return (
